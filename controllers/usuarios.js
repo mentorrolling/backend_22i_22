5f75c3fbd0451cac215db40306dc8ebae39b16e4
pmarino2013 <pmarino2013@gmail.com>
2023-03-08 17:28:29 -0300

    Modelo de usuarios, creacion de usuarios en BD y validaciones de post

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,8 @@
 const { response, request } = require("express");
+// const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs"); //importar librería para encriptar contraseña
+
+const Usuario = require("../models/usuario");
 
 const usuariosGet = (req = request, res = response) => {
   const { apiKey, limit } = req.query;
@@ -10,14 +14,34 @@ const usuariosGet = (req = request, res = response) => {
   });
 };
 
-const usuariosPost = (req = request, res = response) => {
+const usuariosPost = async (req = request, res = response) => {
+  //validar los errores
+  // const errors = validationResult(req);
+  // if (!errors.isEmpty()) {
+  //   return res.status(400).json(errors);
+  // }
+
   //recibir el cuerpo de la petición
-  const { nombre, correo } = req.body;
+  const datos = req.body;
+  const { nombre, correo, password, rol } = datos;
+  const usuario = new Usuario({ nombre, correo, password, rol });
+  //verificar el correo
+  const existeEmail = await Usuario.findOne({ correo });
+  if (existeEmail) {
+    return res.status(400).json({
+      msg: "El correo ya existe",
+    });
+  }
+  //encriptar la contraseña
+  const salt = bcrypt.genSaltSync(10);
+  // const hash = bcrypt.hashSync(password, salt);
+  usuario.password = bcrypt.hashSync(password, salt);
+  //guardar en la BD
+  await usuario.save();
 
   res.json({
-    mensaje: "Post Usuarios",
-    nombre,
-    correo,
+    usuario,
+    message: "Usuario creado correctamente",
   });
 };
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,4 +1,7 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar_campos");
+
 const {
   usuariosGet,
   usuariosPost,
@@ -10,7 +13,20 @@ const router = Router();
 
 router.get("/", usuariosGet);
 
-router.post("/", usuariosPost);
+router.post(
+  "/",
+  [
+    check("nombre", "El nombre es obligatorio").notEmpty(),
+    check(
+      "password",
+      "La contraseña debe tener un mínimo de 6 caracteres"
+    ).isLength({ min: 6 }),
+    check("correo", "No es un correo válido").isEmail(),
+    check("rol", "El rol no es válido").isIn(["USER_ROLE", "ADMIN_ROLE"]),
+    validarCampos,
+  ],
+  usuariosPost
+);
 
 router.put("/:id", usuariosPut);
 
